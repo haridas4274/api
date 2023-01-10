326701ba2843a881ea97efe060e142e2c1107f77
haridas4274 <haridasnamakkal@gmail.com>
2023-01-10 12:59:30 +0530

    add get input method

--- register_api.php
+++ register_api.php
@@ -1,4 +1,7 @@
 <?php
+// get throw json formate:
+json_decode(file_get_contents("php://input"));
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods:POST");
