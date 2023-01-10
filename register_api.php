d0415a529150fa332654ec17d6b167350e3a890c
haridas4274 <haridasnamakkal@gmail.com>
2023-01-10 14:15:02 +0530

    complettion

--- register_api.php
+++ register_api.php
@@ -1,6 +1,6 @@
 <?php
 // get throw json formate:
-json_decode(file_get_contents("php://input"));
+// json_decode(file_get_contents("php://input"));
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -9,16 +9,19 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 
 $db=new mysqli("localhost","root","","users");
+// print_r($_POST);
+
 $name=$_POST['name'];
 $email=$_POST['email'];
 $phone=$_POST['phone'];
 $password=$_POST['password'];
 
 $data[]=array();
-$validate="SELECT * FORM `users_list` WHERE `email`=$email";
-$resulst=$db->query($validate);
+$validate="SELECT * FROM `users_list` WHERE `email`='$email'";
+$result=$db->query($validate);
+
 if($result->num_rows==0){
-    $sql="INSERT INTO `user_list` ('name','email','phone','password')";
+    $sql="INSERT INTO `users_list` (`name`,`email`,`phone`,`password`) VALUES ('$name',$email','$phone','$password')";
     $insert=$db->query($sql);
     $data[]=array("email"=>$email,"name"=>$name);
     $resposnse=array("status"=>true,"msg"=>"succesfully registered","data"=>$data);
